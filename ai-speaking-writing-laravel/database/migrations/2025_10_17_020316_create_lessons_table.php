6b60264efbe7f3f67ae8f8f6af0adc924f00103a
Phạm Minh Trung <111145687+elio-0406@users.noreply.github.com>
2025-10-17 11:18:39 +0700

    init

---
+++ ai-speaking-writing-laravel/database/migrations/2025_10_17_020316_create_lessons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('img_url')->nullable();
+            $table->string('level')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
