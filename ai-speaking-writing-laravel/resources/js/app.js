9caa30e00efabd478d07560134542131e5a2a8e4
Phạm Minh Trung <111145687+elio-0406@users.noreply.github.com>
2025-11-05 02:25:56 +0700

    install vite

--- ai-speaking-writing-laravel/resources/js/app.js
+++ ai-speaking-writing-laravel/resources/js/app.js
@@ -1,1 +1,5 @@
-require('./bootstrap');
+import ('./bootstrap');
+
+console.log("✅ Vite đã hoạt động trong Laravel 8!");
+const testElement = document.getElementsByClassName('test')[0];
+testElement.innerHTML += " - Vite is working!";
