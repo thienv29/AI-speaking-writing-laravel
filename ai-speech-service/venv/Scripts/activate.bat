fc6030b2d52171335de50d8a74566393d6a55cc7
Phạm Minh Trung <111145687+elio-0406@users.noreply.github.com>
2025-11-10 12:08:46 +0700

    test ai-speech-service

--- ai-speech-service/venv/Scripts/activate.bat
+++ ai-speech-service/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\xampp\htdocs\AI-speaking-writing-laravel\ai-speech-service\venv
+set VIRTUAL_ENV=C:\laragon\www\AI-speaking-writing-laravel\ai-speech-service\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
