93dbbff25cacad29ede895d6491b35d6837ad2cd
Phạm Minh Trung <111145687+elio-0406@users.noreply.github.com>
2025-11-10 10:58:37 +0700

    include link css in main layout

--- ai-speaking-writing-laravel/resources/views/layouts/main.blade.php
+++ ai-speaking-writing-laravel/resources/views/layouts/main.blade.php
@@ -8,9 +8,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     @if (app()->environment('local'))
         <!-- Khi dev, load từ server Vite -->
+        <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css">
         <script type="module" src="http://localhost:5173/resources/js/app.js"></script>
     @else
         <!-- Khi build, load file đã compile -->
+        <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
         <script type="module" src="{{ asset('build/assets/app.js') }}"></script>
     @endif
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.0/dist/confetti.browser.min.js"></script>
